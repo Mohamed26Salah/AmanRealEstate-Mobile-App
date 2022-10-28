199a3cce59b99eacdb83eeea78107fba342a25eb
yonosbahrawy <76685173+yonosbahrawy@users.noreply.github.com>
2022-10-28 14:57:05 +0200

    validation

---
+++ aman_mobile/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Y Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\YONOS\\Flutter\\AmanRealEstate-Mobile-App\\aman_mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Y Flutter\\flutter"
+  "PROJECT_DIR=C:\\YONOS\\Flutter\\AmanRealEstate-Mobile-App\\aman_mobile"
+  "FLUTTER_ROOT=C:\\Users\\DELL\\Y Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\YONOS\\Flutter\\AmanRealEstate-Mobile-App\\aman_mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\YONOS\\Flutter\\AmanRealEstate-Mobile-App\\aman_mobile"
+  "FLUTTER_TARGET=C:\\YONOS\\Flutter\\AmanRealEstate-Mobile-App\\aman_mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\YONOS\\Flutter\\AmanRealEstate-Mobile-App\\aman_mobile\\.dart_tool\\package_config.json"
+)
