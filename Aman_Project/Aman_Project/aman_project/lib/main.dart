2e3d1de9457b058862482708ebc84c953e2e7462
Mohamed Salah <mohamedsalahomran44@gmail.com>
2022-11-18 15:32:12 +0200

    FireBase

--- Aman_Project/Aman_Project/aman_project/lib/main.dart
+++ Aman_Project/Aman_Project/aman_project/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:aman_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import './widgets/login_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
--- Aman_Project/Aman_Project/aman_project/lib/widgets/rent.dart
+++ Aman_Project/Aman_Project/aman_project/lib/widgets/rent.dart
@@ -13,157 +13,143 @@ class Rents extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: ListView(
-            
             children: [
               Card(
-                
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RentType(rentID: 2),
-                          ),
-                        );
-                      },
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Image.asset("assets/images/dont-touch.png"),
-                        ),
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RentType(rentID: 2),
+                      ),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Image.asset("assets/images/dont-touch.png"),
                       ),
                     ),
-                    title: Text(
-                      "Didn't Pay",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: const Text(
-                      "2 Items",
-                    ),
-                    
+                  ),
+                  title: Text(
+                    "Didn't Pay",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: const Text(
+                    "2 Items",
                   ),
                 ),
-
-                Card(
-                
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RentType(rentID: 1),
-                          ),
-                        );
-                      },
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Image.asset("assets/images/paid.png"),
-                        ),
+              ),
+              Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RentType(rentID: 1),
+                      ),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Image.asset("assets/images/paid.png"),
                       ),
                     ),
-                    title: Text(
-                      "Paid",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: const Text(
-                      "20 Items",
-                    ),
-                    
+                  ),
+                  title: Text(
+                    "Paid",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: const Text(
+                    "20 Items",
                   ),
                 ),
-
-                Card(
-                
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    
-                    onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RentType(rentID: 4),
-                          ),
-                        );
-                      },
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Image.asset("assets/images/dead 2.png"),
-                        ),
+              ),
+              Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RentType(rentID: 4),
+                      ),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Image.asset("assets/images/dead 2.png"),
                       ),
                     ),
-                    title: Text(
-                      "Contract Old",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: const Text(
-                      "15 Items",
-                    ),
-                    
+                  ),
+                  title: Text(
+                    "Contract Old",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: const Text(
+                    "15 Items",
                   ),
                 ),
-
-                Card(
-                
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RentType(rentID: 3),
-                          ),
-                        );
-                      },
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Image.asset("assets/images/calendar.png"),
-                        ),
+              ),
+              Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RentType(rentID: 3),
+                      ),
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.black,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Image.asset("assets/images/calendar.png"),
                       ),
                     ),
-                    title: Text(
-                      "New Contract",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: const Text(
-                      "7 Items",
-                    ),
-                    
+                  ),
+                  title: Text(
+                    "New Contract",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: const Text(
+                    "7 Items",
                   ),
                 ),
+              ),
             ],
           ),
         ),
