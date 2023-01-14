e5141743cc756723b04288240d8e1954e2d4074c
Mohamed Salah <mohamedsalahomran44@gmail.com>
2023-01-14 19:01:50 +0200

    Revert "Update custom_loading_screen.dart"
    This reverts commit 48aa28aaee7a050dacff008cceb05a19bce7f913.

--- Aman_Project/Aman_Project/aman_project/lib/presentation/shared_features/custom_loading_screen.dart
+++ Aman_Project/Aman_Project/aman_project/lib/presentation/shared_features/custom_loading_screen.dart
@@ -1,37 +1,15 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:lottie/lottie.dart';
 
-class LoadingScreen extends StatefulWidget {
+class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
-
-  @override
-  State<LoadingScreen> createState() => _LoadingScreenState();
-}
-
-class _LoadingScreenState extends State<LoadingScreen>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController ctrl;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    ctrl = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 900));
-  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.square(
-      dimension: max(MediaQuery.of(context).size.shortestSide, 70),
       child: LottieBuilder.asset(
         "assets/Animations/Comp 1.json",
-        controller: ctrl,
-        onLoaded: (p0) {
-          ctrl.forward();
-        },
       ),
     );
     return LoadingAnimationWidget.dotsTriangle(
