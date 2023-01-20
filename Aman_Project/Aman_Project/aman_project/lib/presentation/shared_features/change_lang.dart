7e2fd00dadecb9da03e510a0186022b6b68b6f17
yousefhussen <64665090+yousefhussen@users.noreply.github.com>
2023-01-20 07:29:36 +0200

    Language preference saved
    and added a mounted condition in login loading

--- Aman_Project/Aman_Project/aman_project/lib/presentation/shared_features/change_lang.dart
+++ Aman_Project/Aman_Project/aman_project/lib/presentation/shared_features/change_lang.dart
@@ -1,13 +1,8 @@
+import 'package:aman_project/data/user_management.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../../constants/globals.dart' as glob;
 
 void buildLanguageDialog(BuildContext context) async {
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  await preferences.setInt('counter', glob.x);
-  // await prefs.setStringList('name', <String>['ENGLISH', 'العربيه']);
-
   final List locale = [
     {'name': 'ENGLISH', 'locale': const Locale('en', 'US')},
     {'name': 'العربيه', 'locale': const Locale('ar', 'EG')},
@@ -27,20 +22,13 @@ void buildLanguageDialog(BuildContext context) async {
             child: ListView.separated(
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
-                  glob.x = index;
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: GestureDetector(
-                      child: Text(locale[glob.x]['name']),
+                      child: Text(locale[index]['name']),
                       onTap: () {
-                        var counter = preferences.getInt('counter');
-                        if (counter == 0) {
-                          glob.x = 1;
-                          updateLanguage(locale[1]['locale']);
-                        } else {
-                          glob.x = 0;
-                          updateLanguage(locale[0]['locale']);
-                        }
+                        UserHelper().saveLangPref(index);
+                        updateLanguage(locale[index]['locale']);
                       },
                     ),
                   );
